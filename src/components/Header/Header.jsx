f375e3e8830f7a02a8b928d8ae97974c9c663d00
NeyrowZ <neyrowz@outlook.fr>
2025-08-29 19:01:21 +0200

    Initial Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    const [isActive, setIsActive] = useState(false);
+    return (
+        <header>
+            <div className="container">
+                <Link to="/" className="title">
+                    <img src="/imgs/logo.png" alt="Elisa Bitmoji"/>
+                    <h1>Epsilon</h1>
+                </Link>
+                <nav>
+                    <ul>
+                        <li><Link to="/"><h1>Accueil</h1></Link></li>
+                        <li><Link to="/projects"><h1>Projets</h1></Link></li>
+                    </ul>
+                </nav>
+                <Link onClick={() => setIsActive(!isActive)} className="burger"><i class="fa-solid fa-bars"></i></Link>
+                <nav className={`menu${isActive ? ' active' : ''}`}>
+                    <ul>
+                        <li><Link onClick={() => setIsActive(false)} to="/"><h1>Accueil</h1></Link></li>
+                        <li><Link onClick={() => setIsActive(false)} to="/projects"><h1>Projets</h1></Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
